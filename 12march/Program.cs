2527f1e46427894210d517d53e37c458570f6296
unknown <you@example.com>
2025-03-11 15:01:28 -0700

    6-ci task hazirdir

--- 12march/Program.cs
+++ 12march/Program.cs
@@ -20,13 +20,21 @@
 //else { Console.WriteLine("daxil olunan ededler yalnishdir!"
 //    ); }
 
+using System.ComponentModel.Design;
+
 ;
 
-int num = 89;
-if(num >=91 )
-{ Console.WriteLine("A"); }
-else if (num >= 81)
+//int num = 89;
+//if(num >=91 )
+//{ Console.WriteLine("A"); }
+//else if (num >= 81)
+//{
+//    Console.WriteLine("B");
+//}
+//else { Console.WriteLine("C"); }
+
+int num=28;
+if ((num % 10) > (num / 10))
 {
-    Console.WriteLine("B");
-}
-else { Console.WriteLine("C"); }
+    Console.WriteLine("teklik mertebesi boyukdur");
+}else { Console.WriteLine("onluq mertebesi boyukdur"); }
--- 12march/obj/Debug/net8.0/12march.AssemblyInfo.cs
+++ 12march/obj/Debug/net8.0/12march.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("12march")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620cf4b3dd0e16ce9d386f47f4cd3cacf0575043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62683645851e1ac3d560847483a34698747272d")]
 [assembly: System.Reflection.AssemblyProductAttribute("12march")]
 [assembly: System.Reflection.AssemblyTitleAttribute("12march")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

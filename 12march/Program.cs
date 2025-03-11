6f06795c751bf5ade4eea3f76477dbfad0b22ad8
unknown <you@example.com>
2025-03-11 14:08:09 -0700

    2-ci task hazirdir

---
+++ 12march/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//string username="admin", password ="admin123";
+//    if (username == "admin" && password =="admin123")
+//{ Console.WriteLine("xosh gelmisiniz!"); }
+//else { Console.WriteLine("username ve ya password yalnishdir"); }
+int num = 4;
+if (num%2==0)
+{ Console.WriteLine("eded cutdur"); }
+else { Console.WriteLine("eded tekdir"
+    ); }
+
